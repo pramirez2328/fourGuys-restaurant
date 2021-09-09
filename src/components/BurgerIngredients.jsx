124a075b4d33acaf8963f8230112d9d275a23ee4
ramirez <prlara2000@gmail.com>
2021-09-08 22:24:08 -0500

    fix interface burger page

--- src/components/BurgerIngredients.jsx
+++ src/components/BurgerIngredients.jsx
@@ -3,69 +3,86 @@ import "../style/burgerIngredients.css";
 
 function BurgerIngredients({ value }) {
   return (
-    <div id="burgerIngredients">
-      <div>
-        <h3>Lettuce</h3>
-        <label className="switch">
-          <input type="checkbox" value={`${value}`} />
-          <span className="slider round"></span>
-        </label>
+    <div id="ingredients">
+      <h2 class="subtitle">Customize your burger!</h2>
+      <div className="foodIngredients">
+        <div>
+          <h3> Jalapeño</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <div>
+          <h3>Cheese</h3>
+          <label className="switch">
+            <input type="checkbox" value={`${value}`} />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <div>
+          <h3>Lettuce</h3>
+          <label className="switch">
+            <input type="checkbox" value={`${value}`} />
+            <span className="slider round"></span>
+          </label>
+        </div>
+
+        <div>
+          <h3>Tomato</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+
+        <div>
+          <h3>Onions</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
       </div>
 
-      <div>
-        <h3> Jalapeño</h3>
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
+      <div className="foodIngredients">
+        <div>
+          <h3>Mushroom</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <div>
+          <h3>Mayo</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <div>
+          <h3>Ketchup</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
 
-      <div>
-        <h3>Tomato</h3>
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
+        <div>
+          <h3>Mustard</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
 
-      <div>
-        <h3>Onions</h3>
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
-
-      <div>
-        <h3>Mayo</h3>
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
-
-      <div>
-        <h3>Ketchup</h3>
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
-
-      <div>
-        <h3>Mustard</h3>
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
-      </div>
-
-      <div>
-        <h3>Relish</h3>
-        <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-        </label>
+        <div>
+          <h3>Relish</h3>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+        </div>
       </div>
     </div>
   );
--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import SideBar from "./SideBar";
 import Cart from "./Cart";
 import AddItems from "./AddItems";
